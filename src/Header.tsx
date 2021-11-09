fbd3e659da99b94ad57b85ec96b0be4ac7fec440
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2021-11-09 20:31:50 +0300

    first commit

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <a href="#a">Home</a>
+            <a href="#a">News feed</a>
+            <a href="#a">Message</a>
+        </div>
+    )
+}
+
+
+export default Header
