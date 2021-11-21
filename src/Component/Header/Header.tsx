0321e43886f5b571049154a5539fabf8f94548ac
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2021-11-21 17:48:18 +0300

    20. my post

---
+++ src/Component/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './Header.module.css'
+
+export const Header = () => {
+    return (
+        <header className={s.header}>
+            <img src="https://www.logodesign.net/logo/abstract-cuboid-building-4519ld.png" alt="logo"/>
+        </header>
+    )
+}
---
+++ src/Component/Navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import s from './Navbar.module.css'
+import {NavLink, useLocation} from 'react-router-dom';
+
+export const Navbar = () => {
+    const location = useLocation()
+    const {pathname} = location
+    const splitLocation = pathname.split('/')
+
+    return (
+        <nav className={s.nav}>
+            <div className={splitLocation[1] === 'profile' ? s.activeLink : s.item}>
+                <NavLink to={'/profile'}>Profile</NavLink>
+            </div>
+            <div className={splitLocation[1] === 'dialogs' ? s.activeLink : s.item}>
+                <NavLink to={'/dialogs'}>Messages</NavLink>
+            </div>
+            <div className={splitLocation[1] === 'news' ? s.activeLink : s.item}>
+                <NavLink to={'/news'}>News</NavLink>
+            </div>
+            <div className={splitLocation[1] === 'music' ? s.activeLink : s.item}>
+                <NavLink to={'/music'}>Music</NavLink>
+            </div>
+            <div className={splitLocation[1] === 'setting' ? s.activeLink : s.item}>
+                <NavLink to={'setting'}>Setting</NavLink>
+            </div>
+        </nav>
+    )
+}
