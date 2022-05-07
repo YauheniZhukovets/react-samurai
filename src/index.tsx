9ac0a97ffd1fb826deddca80deaf9e5ff6d966ec
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2022-05-07 12:23:58 +0300

    fixed BrowserRouter. Added basename for BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import {store} from './Redux/reduxStore';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <App/>
             </Provider>
